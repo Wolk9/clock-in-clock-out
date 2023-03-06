5557cf0c09b78ae51bac56e6d549baccd6a5d21c
Martin laptop <info@setisbew.nl>
2023-03-06 10:33:20 +0100

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { configureStore } from 'redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  // hier komt jouw Firebase-configuratie
+  apiKey: "AIzaSyAfS-I1miPzZZA5Mc9Knj6oKIQw_C-dsUk",
+  authDomain: "clockinout-wolk9.firebaseapp.com",
+  projectId: "clockinout-wolk9",
+  storageBucket: "clockinout-wolk9.appspot.com",
+  messagingSenderId: "795930777692",
+  appId: "1:795930777692:web:418b79366fdb7ab9772e99",
+  measurementId: "G-F7DN5X0377"
+};
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
+firebase.initializeApp(firebaseConfig);
+
+const store = configureStore({
+  reducer: {
+    clockInOut: clockInOutReducer,
+  },
+});
+
 
 root.render(
   <React.StrictMode>
@@ -17,7 +38,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
